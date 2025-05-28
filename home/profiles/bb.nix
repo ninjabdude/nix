3fa76103f111ee50e0671276f74c2ca3f349fe42
ninjabdude <47196997+ninjabdude@users.noreply.github.com>
2025-05-28 12:47:53 -0600

    testing mkOutOfStoreSymlink

--- home/profiles/bb.nix
+++ home/profiles/bb.nix
@@ -1,10 +1,16 @@
 {
-  homeDirectory,
-  username,
-  system,
-}: {pkgs, ...}: {
-  home.homeDirectory = homeDirectory;
-  home.username = username;
-  home.packages = with pkgs; [fastfetch home-manager];
-  home.stateVersion = "24.11";
+    homeDirectory,
+	username,
+	system,
+}: {config, lib, pkgs, ...}: {
+    home.homeDirectory = homeDirectory;
+    home.username = username;
+    home.packages = with pkgs; [fastfetch home-manager tmux];
+    home.stateVersion = "24.11";
+
+
+
+    # editMode = true;
+    home.file."${config.home.homeDirectory}/.config/tmux/tmux.conf".source = 
+	config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix/home/modules/tmux/tmux.conf";
 }
