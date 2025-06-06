c196c1d3eb1a474986a15d38a7abe634ced78a6b
ninjabdude <47196997+ninjabdude@users.noreply.github.com>
2025-06-06 17:08:37 -0600

    zoxide now uses cd and cdi

--- home/bundles/terminal.nix
+++ home/bundles/terminal.nix
@@ -4,11 +4,10 @@
 	../modules/tmux/tmux.nix			
 	../modules/zsh/zsh.nix
 	../modules/starship.nix
-	../modules/zoxide.nix
 	../modules/fzf.nix
+	../modules/zoxide.nix
 	../modules/sesh.nix
 	# ../modules/ghostty/ghostty.nix
-
 	../modules/wezterm/wezterm.nix
     ];
 }
