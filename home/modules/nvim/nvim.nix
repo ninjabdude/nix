66dbf5343f8f637f67eba27abd55ba91e3b216c5
ninjabdude <47196997+ninjabdude@users.noreply.github.com>
2025-06-08 13:35:51 -0600

    set up 2 packageDefinitions, stable and test with unique lua files

--- home/modules/nvim/nvim.nix
+++ home/modules/nvim/nvim.nix
@@ -19,7 +19,7 @@ in {
       ];
       # see the packageDefinitions below.
       # This says which of those to install.
-      packageNames = [ "myHomeModuleNvim" ];
+      packageNames = [ "stable" "test"];
 
       luaPath = ./.;
 
@@ -133,7 +133,7 @@ in {
       packageDefinitions.replace = {
         # These are the names of your packages
         # you can include as many as you wish.
-        myHomeModuleNvim = {pkgs, name, ... }: {
+        stable = {pkgs, name, ... }: {
           # they contain a settings set defined above
           # see :help nixCats.flake.outputs.settings
           settings = {
@@ -143,7 +143,7 @@ in {
             unwrappedCfgPath = "${homeLib.homeDirectory}/nix/home/modules/nvim";
             # IMPORTANT:
             # your alias may not conflict with your other packages.
-            aliases = [ "vim" "homeVim" ];
+            aliases = [ "homeVim" ];
             # neovim-unwrapped = inputs.neovim-nightly-overlay.packages.${pkgs.system}.neovim;
             hosts.python3.enable = true;
             hosts.node.enable = true;
@@ -162,6 +162,37 @@ in {
             nixdExtras.nixpkgs = ''import ${pkgs.path} {}'';
           };
         };
+	
+        test = {pkgs, name, ... }: {
+          # they contain a settings set defined above
+          # see :help nixCats.flake.outputs.settings
+          settings = {
+            suffix-path = true;
+            suffix-LD = true;
+            wrapRc = !homeLib.editMode;
+            unwrappedCfgPath = "${homeLib.homeDirectory}/nix/home/modules/nvim";
+            # IMPORTANT:
+            # your alias may not conflict with your other packages.
+            aliases = [ "testVim" ];
+            # neovim-unwrapped = inputs.neovim-nightly-overlay.packages.${pkgs.system}.neovim;
+            hosts.python3.enable = true;
+            hosts.node.enable = true;
+          };
+          # and a set of categories that you want
+          # (and other information to pass to lua)
+          # and a set of categories that you want
+          categories = {
+            general = true;
+            lua = true;
+            nix = true;
+            go = false;
+          };
+          # anything else to pass and grab in lua with `nixCats.extra`
+          extra = {
+            nixdExtras.nixpkgs = ''import ${pkgs.path} {}'';
+          };
+        };
+
       };
     };
   };
