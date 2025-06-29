5788796f0952c00a8e3569dc5b37bb19d7030e9e
ninjabdude <47196997+ninjabdude@users.noreply.github.com>
2025-06-29 17:44:06 -0600

    got rid of tmux-which-key

--- home/configs/tmux/tmux.nix
+++ home/configs/tmux/tmux.nix
@@ -6,22 +6,10 @@
 	    source = "${homeLib.homeDirectory}/nix/home/configs/tmux/custom"; 
 	    inherit lib config;
 	};
-    xdg.configFile = {
-	"tmux/plugins/tmux-which-key/config.yaml".text = lib.generators.toYAML {} {
-	    command_alias_start_index = 200;
-	    # rest of config here
-	};
-    };
     programs.tmux = {
 	enable = true;
 	extraConfig = "source-file ${homeLib.homeDirectory}/.config/tmux/custom/tmux.conf";
 	plugins = with pkgs; [
-	    {
-		plugin = tmuxPlugins.tmux-which-key;
-		extraConfig = ''
-		    set -g @tmux-which-key-xdg-enable 1;
-		'';
-	    }
 	];
     };
     programs.sesh = {
