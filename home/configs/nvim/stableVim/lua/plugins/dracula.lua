a2b279d0917a9eab2126cf06908cd548c94981f8
ninjabdude <47196997+ninjabdude@users.noreply.github.com>
2025-07-11 15:20:34 -0600

    nvim color changes

--- home/configs/nvim/stableVim/lua/plugins/dracula.lua
+++ home/configs/nvim/stableVim/lua/plugins/dracula.lua
@@ -14,5 +14,7 @@ return {
         vim.api.nvim_set_hl(0, "MatchParen", { bg = "#a688ff", fg = "#000000", bold = true })
         vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#1c1c1c" })
         vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1c1c1c" })
+	vim.api.nvim_set_hl(0, "@type.builtin", { fg = "#DD6E6B" })
+	vim.api.nvim_set_hl(0, "@keyword.function", { fg = "#E48CC1" })
     end,
 }
